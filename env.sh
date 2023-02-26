53b58aca8061c1b245e2fd05fe7819806ac2a03f
EugeneSeo <eugene.s21@kaist.ac.kr>
2023-02-26 17:15:51 +0000

    Fixed bugs and added readme

--- env.sh
+++
@@ -1,7 +0,0 @@
-conda activate h_face
-
-# cuda path
-export PATH="/usr/local/cuda-11.6/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/cuda-11.6/lib64:$LD_LIBRARY_PATH"
-export PYTHONPATH="${PYTHONPATH}:/home/egseo/hugging_face/transformers/src/transformers/bfp_training"
-export BFP_HOME="/home/egseo/hugging_face/transformers/src/transformers/bfp_training"
